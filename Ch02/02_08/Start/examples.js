51c5a58fd951f3b59f494eab05e019349835b471
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-18 21:45:26 +0200

    Higher-order functions

--- Ch02/02_08/Start/examples.js
+++ Ch02/02_08/Start/examples.js
@@ -1,1 +1,16 @@
+const divide = (x, y) => x / y;
 
+const secondArgumentIsNotZero = func =>
+  (...args) => {
+    if (args[1] === 0) {
+      console.log("Dividing by 0");
+      return null;
+    }
+
+    return func(...args);
+  };
+
+const divideSafe = secondArgumentIsNotZero(divide);
+
+console.log(divideSafe(7, 0));
+console.log(divideSafe(6, 3));
