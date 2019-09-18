81fb8c66de92430b64b207a23dafb70816b7409a
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-18 20:53:30 +0200

    Passing functions as arguments

--- Ch02/02_04/Start/examples.js
+++ Ch02/02_04/Start/examples.js
@@ -1,2 +1,13 @@
 const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
+
+const combine2And3 = func => func(2, 3);
+
+combine2And3(add); // 5
+combine2And3(subtract); // -1
+
+const result = combine2And3(Math.max);
+console.log(result);
+
+const result2 = combine2And3((x, y) => x * y);
+console.log(result2);
