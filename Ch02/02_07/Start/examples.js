344cf68b0a7c53d6c442aebae9991dabb5d86838
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-18 21:34:12 +0200

    Implement private variable

--- Ch02/02_07/Start/examples.js
+++ Ch02/02_07/Start/examples.js
@@ -1,1 +1,25 @@
+const Person = ({ name, age, job }) => {
+  var _name = name;
+  var _age = age;
+  var _job = job;
 
+  return {
+    getName: () => _name,
+    getAge: () => _age,
+    getJob: () => _job,
+    setJob: newJob => _job = newJob,
+  };
+};
+
+const me = Person({
+  name: 'Jon',
+  age: 25,
+  job: 'developer',
+});
+
+console.log(me.getName());
+console.log(me.getJob());
+
+me.setJob('senior dev');
+
+console.log(me.getJob());
