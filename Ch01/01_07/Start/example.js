4fc5cffd2d28632ca452a49b5f53c0b572331b36
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-18 19:59:12 +0200

    Arrow Functions

--- Ch01/01_07/Start/example.js
+++ Ch01/01_07/Start/example.js
@@ -1,10 +1,3 @@
 const sayHello = name => console.log(`Hello ${name}`);
 
 sayHello('Shaun');
-
-const person = {
-  name: 'Joe',
-  age: 34,
-};
-
-person.name = 'aaa';
--- Ch02/02_01/Start/examples.js
+++ Ch02/02_01/Start/examples.js
@@ -1,1 +1,19 @@
+function myFunction (arg1, arg2) {
+  // code
+}
 
+const myOtherFunction = function (arg1, arg2) {
+  // code
+};
+
+const add = (x, y) => x + y;
+
+const double = number => number * 2;
+
+const sayHello = () => console.log('Hello');
+
+const getPersonData = () => ({
+  name: 'Jon',
+  age: 43,
+  job: 'programmer',
+});
