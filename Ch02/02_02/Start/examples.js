0f740413a2d6b4446b11e4e87602621e8fce425e
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-18 20:34:21 +0200

    Functions as data

--- Ch02/02_02/Start/examples.js
+++ Ch02/02_02/Start/examples.js
@@ -1,1 +1,25 @@
+const sayHello = name => console.log(`Hello ${name}`);
+
+const sayHello2 = sayHello;
+
+sayHello2('Jon');
+
+const myFunction = true
+  ? () => console.log('First option')
+  : () => console.log('Second option');
+
+const DEVELOPMENT = true;
+
+const fetchDataReal = () => {
+  // operation
+};
+
+const fetchDataFake = () => ({
+  name: 'Jon',
+  age: 35,
+});
+
+const fetchData = DEVELOPMENT
+  ? fetchDataFake()
+  : fetchDataReal();
 
