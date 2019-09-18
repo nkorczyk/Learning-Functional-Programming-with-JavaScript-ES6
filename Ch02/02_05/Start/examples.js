90b62393172db6760ff6db45928781c896c1ad33
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-18 21:11:57 +0200

    Returning functions

--- Ch02/02_05/Start/examples.js
+++ Ch02/02_05/Start/examples.js
@@ -1,1 +1,19 @@
+const createPrinterFirst = function() {
+  return function() {
+    console.log('Hello');
+  }
+}
 
+const createPrinter = () => () => console.log('Hello');
+
+// const double = x => x * 2;
+// const triple = x => x * 3;
+// const quadruple = x => x * 4;
+
+const createMultiplier = y => x => x * y;
+
+const double = createMultiplier(2);
+const triple = createMultiplier(3);
+const quadruple = createMultiplier(4);
+
+double(3);
