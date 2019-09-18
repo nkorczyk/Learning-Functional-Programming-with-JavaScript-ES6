e23731ac0a98a34c84807deac702418db15ee495
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-18 22:47:17 +0200

    The spread operator

--- Ch03/03_02/Start/examples.js
+++ Ch03/03_02/Start/examples.js
@@ -1,11 +1,38 @@
 const person = {
-    name: 'Jimmy Smith',
-    age: 40,
-    hairColor: 'brown',
-    eyeColor: 'blue',
+  name: 'Jimmy Smith',
+  age: 40,
+  hairColor: 'brown',
+  eyeColor: 'blue',
 };
 
 const careerData = {
-    title: 'developer',
-    company: 'ABC Software',
+  name: 'Bob',
+  title: 'developer',
+  company: 'ABC Software',
 };
+
+const personWithCareer = {
+  ...person,
+  ...careerData,
+};
+
+console.log(personWithCareer);
+
+const updates = {
+  name: 'James',
+};
+
+const updatedPerson = {
+  ...person,
+  ...updates,
+};
+
+console.log(updatedPerson);
+
+const numbers = [1, 2, 3, 4, 5];
+const newNumbers = [
+  ...numbers,
+  6,
+];
+
+console.log(newNumbers);
