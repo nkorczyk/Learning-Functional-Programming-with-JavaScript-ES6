d624fef3e0cb9e1faa3e29f14e961d70262ee0ee
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-19 20:39:18 +0200

    Sorting

--- Ch03/03_07/Start/examples.js
+++ Ch03/03_07/Start/examples.js
@@ -1,1 +1,39 @@
 const mixedUpNumbers = [10, 2, 4, 3, 7, 5, 8, 9, 1, 6];
+
+const ascending = (a, b) => {
+  if (a < b) return -1;
+  if (a > b) return 1;
+  return 0;
+};
+
+const sortedNumbers = mixedUpNumbers.slice().sort(ascending);
+
+console.log(sortedNumbers);
+
+const descending = (a, b) => {
+  if (a > b) return -1;
+  if (a < b) return 1;
+  return 0;
+};
+
+const sortedNumbersDescending = mixedUpNumbers.slice().sort(descending);
+
+console.log(sortedNumbersDescending);
+
+// HOC
+const sortFactory = type => (a, b) => {
+  if (type === 'ascending') {
+    if (a < b) return -1;
+    if (a > b) return 1;
+    return 0;
+  }
+  if (type === 'descending') {
+    if (a > b) return -1;
+    if (a < b) return 1;
+    return 0;
+  }
+};
+
+const sortedNum = mixedUpNumbers.slice().sort(sortFactory('descending'));
+
+console.log('Sorted numbers', sortedNum);
