d249821af8740bd868180a51c676a5bc1c3e8744
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-19 20:20:32 +0200

    Every/Some

--- Ch03/03_05/Start/examples.js
+++ Ch03/03_05/Start/examples.js
@@ -1,14 +1,32 @@
 const employees = [{
-    name: 'Jane Doe',
-    salary: 90000,
+  name: 'Jane Doe',
+  salary: 90000,
 }, {
-    name: 'Donald Jones',
-    salary: 65000,
+  name: 'Donald Jones',
+  salary: 65000,
 }, {
-    name: 'Donna Appleseed',
-    salary: 1500000,
+  name: 'Donna Appleseed',
+  salary: 1500000,
 }, {
-    name: 'John Smith',
-    salary: 250000,
+  name: 'John Smith',
+  salary: 250000,
 }];
 
+const makesMoreThanOneMillion = employee => employee.salary > 1000000;
+
+const result = employees.some(makesMoreThanOneMillion);
+
+console.log(result);
+
+const formValues = [
+  'Jon',
+  'Wassel',
+  'Maine',
+  '',
+];
+
+const isNotEmpty = string => !!string;
+
+const allFieldsFilled = formValues.every(isNotEmpty);
+
+console.log(allFieldsFilled);
