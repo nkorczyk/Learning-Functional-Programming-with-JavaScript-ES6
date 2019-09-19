492329357590089dc242dea9e3b548f49bec3966
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-19 23:45:45 +0200

    Convert array

--- Ch05/05_01/Start/challenge.js
+++ Ch05/05_01/Start/challenge.js
@@ -1,14 +1,16 @@
 const electionVotes = [
-    'Harry', 'Rick', 'Ben', 'Ben', 'Harry', 'Ashley',
-    'Connor', 'Rick', 'Ashley', 'Rick', 'Albert', 'Ben',
-    'Michael', 'Rick', 'Albert', 'Karen', 'Harry',
-    'Karen', 'Donna', 'Ashley', 'Albert', 'Harry',
-    'Dane', 'Dane', 'Rick', 'Donna', 'Mortimer',
+  'Harry', 'Rick', 'Ben', 'Ben', 'Harry', 'Ashley',
+  'Connor', 'Rick', 'Ashley', 'Rick', 'Albert', 'Ben',
+  'Michael', 'Rick', 'Albert', 'Karen', 'Harry',
+  'Karen', 'Donna', 'Ashley', 'Albert', 'Harry',
+  'Dane', 'Dane', 'Rick', 'Donna', 'Mortimer',
 ];
 
-const tallyVotes = votes => {
-    // Your code here
-};
+const tallyVotes = votes =>
+  votes.reduce((acc, name) => ({
+    ...acc,
+    [name]: acc[name] ? acc[name] + 1 : 1,
+  }), {});
 
 console.log(tallyVotes(electionVotes));
 
