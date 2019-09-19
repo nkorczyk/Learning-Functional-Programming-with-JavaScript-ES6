4eaa03a02d74cb2fbe07abb51bdea6b4667267a4
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-19 20:27:07 +0200

    Slicing

--- Ch03/03_06/Start/examples.js
+++ Ch03/03_06/Start/examples.js
@@ -1,1 +1,13 @@
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// numbers.sort(); // mutation
+// numbers.reverse(); // mutation
+// numbers.push(8); // mutation
+// numbers.pop(); // mutation
+
+console.log(numbers.slice(3, 8));
+console.log(numbers.slice().reverse());
+console.log(numbers);
+
+console.log([...numbers].reverse());
+console.log(numbers);
