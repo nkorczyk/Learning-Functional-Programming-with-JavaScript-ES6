af6e3b078ba41b9647311d964e9ba720a5379340
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-19 20:49:40 +0200

    Reducing

--- Ch03/03_08/Start/examples.js
+++ Ch03/03_08/Start/examples.js
@@ -1,1 +1,20 @@
 const numbers = [5, 7, 2, 40, 23, 14, 8, 4, 11];
+
+console.log(numbers);
+
+const sum = numbers.reduce((acc, element) => {
+  console.log(`acc is ${acc}`);
+  console.log(`element is ${element}`);
+  return acc + element;
+}, 0);
+
+console.log(sum);
+
+
+const product = numbers.reduce((acc, element) => {
+  console.log(`acc is ${acc}`);
+  console.log(`element is ${element}`);
+  return acc * element;
+}, 1);
+
+console.log(product);
