fb4b1d20ac726fedc9f5461b8f7cfeff34dca257
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-19 20:06:37 +0200

    Filtering

--- Ch03/03_04/Start/examples.js
+++ Ch03/03_04/Start/examples.js
@@ -1,1 +1,14 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const evenNumbers = numbers.filter(x => x % 2 === 0);
+
+console.log(evenNumbers);
+
+const words = ['hello', 'goodbye', 'the', 'Antarctica'];
+
+const createLengthTest = minLength =>
+  word => word.length > minLength;
+
+const longWords = words.filter(createLengthTest(5));
+
+console.log(longWords);
