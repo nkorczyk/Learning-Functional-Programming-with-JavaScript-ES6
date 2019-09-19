733f9cc7954c3c6cf1cbcc3b153ebaf068a306e6
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-19 21:47:42 +0200

    Currying

---
+++ Ch04/04_02/Start/examples.js
@@ -0,0 +1,30 @@
+// const add = (x, y, z) => x + y + z;
+// const add5 = (y, z) => 5 + y + z;
+
+const add = (x, y, z) => x + y + z;
+
+// const addPartial = x =>
+//   (y, z) => add(x, y, z);
+
+// const add5 = addPartial(5);
+// const sum = add5(6, 7);
+
+// console.log(sum);
+
+// const addPartial = (x, y) =>
+//   z => add(x, y, z);
+
+// const add5and6 = addPartial(5, 6);
+// const sum = add5and6(7);
+
+// console.log(sum);
+
+const addPartial = x => y => z => add(x, y, z);
+
+const add5 = addPartial(5);
+const add5And6 = add5(6);
+const sum = add5And6(7);
+console.log(sum);
+
+const result = addPartial(5)(6)(7);
+console.log(result);
