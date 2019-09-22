d93e97cdd686ef72ba2c15c3ff66517e4620ec57
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-22 19:23:59 +0200

    Error messages

--- Ch05/05_05/Start/challenge.js
+++ Ch05/05_05/Start/challenge.js
@@ -1,19 +1,41 @@
 const currentInputValues = {
-    firstName: '', // Must be at least 2 characters
-    lastName: '', // Must be at least 2 characters
-    zipCode: '', // Must be exactly 5 characters
-    state: '', // Must be exactly 2 characters
+  firstName: 'John', // Must be at least 2 characters
+  lastName: '', // Must be at least 2 characters
+  zipCode: '12345', // Must be exactly 5 characters
+  state: '', // Must be exactly 2 characters
 }
 
 const inputCriteria = {
-    firstName: [],
-    lastName: [],
-    zipCode: [],
-    state: [],
+  firstName: [
+    value => value.length >= 2
+      ? ''
+      : 'First name must be at least 2 characters'
+  ],
+  lastName: [
+    value => value.length >= 2
+      ? ''
+      : 'Last name must be at least 2 characters'
+  ],
+  zipCode: [
+    value => value.length === 5
+      ? ''
+      : 'Zip code must be exactly 5 characters'
+  ],
+  state: [
+    value => value.length === 2
+      ? ''
+      : 'State must be exactly 2 characters'
+  ],
 };
 
 const getErrorMessages = (inputs, criteria) => {
-
+  return Object
+    .keys(inputs)
+    .reduce((acc, fieldName) => [
+      ...acc,
+      ...criteria[fieldName].map(test => test(inputs[fieldName])),
+    ], [])
+    .filter(message => message);
 }
 
 console.log(getErrorMessages(currentInputValues, inputCriteria));
