d4d6bf6becfe3663844626d9a8526e69cea5d3eb
Norbert Korczyk <korczyk.norbert@gmail.com>
2019-09-22 19:06:01 +0200

    Anagrams

--- Ch05/05_03/Start/challenge.js
+++ Ch05/05_03/Start/challenge.js
@@ -1,13 +1,23 @@
 import words from 'an-array-of-english-words';
 
-const findAnagrams = (word, allWords) => {
-    const letters = word.split('');
+const countOccurences = arr =>
+  arr.reduce((acc, str) => ({
+    ...acc,
+    [str]: acc[str] ? acc[str] + 1 : 1,
+  }), {});
 
-    // Your code here
-}
+const hasSameLetterCount = (word1, word2) => {
+  const word1Count = countOccurences(word1.split(''));
+  const word2Count = countOccurences(word2.split(''));
+
+  return Object.keys(word1Count).length === Object.keys(word2Count).length &&
+    Object.keys(word1Count).every(letter => word1Count[letter] === word2Count[letter]);
+};
+
+const findAnagrams = (word, allWords) => {
+  return allWords
+    .filter(entry => hasSameLetterCount(word, entry))
+    .filter(anagram => anagram !== word);
+};
 
 console.log(findAnagrams('cinema', words));
-
-/*
-    Expected output: ['iceman', 'anemic']
-*/
